889f22fee74a9c6058473d1dbfc3e82240aaae17
Rustam Salimbayev <rustam61it@gmail.com>
2022-06-25 17:32:45 +0500

    new

--- doc.c
+++ doc.c
@@ -2,6 +2,18 @@ dsaasd
 a
 defaultasd
 as
+import "fmt"
+
+
+func main (){
+
+	fmt.Println("Salom")
+
+	fmt.Println("qalay")
+	fmt.Println("Salom")
+	fmt.Println("Salom")
+
+}
 __DBL_HAS_DENORM__d
 asd
 as
